e9761fb59154cc3ab26e9fb823bbbca09aefddc6
GeloUno <geloke59@gmail.com>
2018-09-04 14:02:44 +0200

    index.html add to form input title and description, add button delete to all li listToDo. app.js add delete function from FireBase. Add file db.text how to connect to database. app.js add function set data to FireBase

--- app.js
+++ app.js
@@ -1,28 +1,56 @@
+const toDoList = document.querySelector('#to-do-list');
+const addToDoForm = document.querySelector('#add-to-do-form');
+/*
+create element li from db and render this element
+*/
 
-   toDoList = document.querySelector('#to-do-list');
+function createToDoListLi(el) {
+    let li = document.createElement('li');
+    let title = document.createElement('span');
+    let description = document.createElement('span');
+    let cross = document.createElement('button');
 
-   /*
-   create element li from db and render this element
-   */
-   
-   function createToDoListLi(el){
-       let li = document.createElement('li');
-       let title =document.createElement('span');
-       let description =document.createElement('span');
-   
-       li.setAttribute('id',el.id);
-       title.textContent = el.data().title;
-       description.textContent = el.data().description;
-   
-       li.appendChild(title);
-       li.appendChild(description);
-        toDoList.appendChild(li);
-   }
-   
-       db.collection('todoList').get().then((snapshot)=>{
-       snapshot.docs.forEach(element => {
-            console.log(element.data())
-           createToDoListLi(element);
-       });
-   })
-   
+    li.setAttribute('id', el.id);
+    title.textContent = el.data().title;
+    description.textContent = el.data().description;
+    cross.textContent = 'Delete';
+
+    li.appendChild(title);
+    li.appendChild(description);
+    li.appendChild(cross);
+    toDoList.appendChild(li);
+
+    // delete function from evenlistener click button 
+
+    cross.addEventListener('click', e => {
+      //  e.stopPropagation();
+        console.log(e.target.parentElement.getAttribute('id'));
+        let id = e.target.parentElement.getAttribute('id');
+        db.collection('todoList').doc(id).delete().then(()=>{
+            console.log("Delete: " + id);
+        });
+    })
+}
+// geting data from FireBase
+db.collection('todoList').get().then((snapshot) => {
+    snapshot.docs.forEach(element => {
+        console.log(element.data())
+        createToDoListLi(element);
+    });
+})
+// seting data to FireBase
+
+addToDoForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if ((addToDoForm.title.value != '') && (addToDoForm.description.value != '')) {
+        // add colection to the FireBase
+        db.collection('todoList').add({
+            title: addToDoForm.title.value,
+            description: addToDoForm.description.value
+
+        });
+    }
+    addToDoForm.title.value = '';
+    addToDoForm.description.value = '';
+
+});
